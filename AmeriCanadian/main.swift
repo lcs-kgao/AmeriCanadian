9079105c5177ecb1867db30a5fd4f2062caaf9de
Gao Kevin <kgao@lcs-mba3111.lcs.on.ca>
2018-05-22 09:39:22 -0400

    Finish

--- AmeriCanadian/main.swift
+++ AmeriCanadian/main.swift
@@ -10,21 +10,40 @@ import Foundation
 var expectedCountOfWordsToTranslate = 3
 
 // Write a loop to actually collect the expected count of words to be translated
+while true {
+    
+    //Prompt
+    print("How many lines will be provided?")
+    
+    //Test for nil
+    guard let givenInput = readLine() else{
+        continue
+        
+    }
+    //Test for integer
+    guard let givenInteger = Int(givenInput) else {
+        
+        continue
+    }
+    //Check for the correct range
+    if givenInteger < 0 || givenInteger > 10 {
+        continue
+    }
+    //Assign the input value to the global variable
+    expectedCountOfWordsToTranslate = givenInteger
+    break
+}
+
 
 // PROCESS
 // Implement the primary logic of the problem here
 // Some output may be given here if you desire
 
 // NOTE:
-//
-// Some example code that may be useful
+
+//Create variables for later use
 var word = "tour"
-let isAmerican = word.hasSuffix("or")
-print(isAmerican)
-var reversedWord = String(word.reversed())
-print("The reversed word is: \(reversedWord)")
-var originalWord = String(reversedWord.reversed())
-print("The reversed word, reversed again is: \(originalWord)")
+var translatedWord = ""
 
 // Example of how to collect multiple input lines
 for counter in 1...expectedCountOfWordsToTranslate {
@@ -39,10 +58,43 @@ for counter in 1...expectedCountOfWordsToTranslate {
         continue
     }
     
-    // Now we have the line, we can print it out, analyze it as needed, et cetera
-    print(givenInput)
+    //Assign the input to the global variable
+    word = givenInput
+    print(word)
     
+    
+    // if the word contains a suffix "or", it is an American word.
+    let isAmerican = word.hasSuffix("or")
+    print(isAmerican)
+    
+    //If is is not an American word, print the result
+    if isAmerican == false{
+        print("The translation is \(word)")
+    }else if word.count < 4 { //If the word has less than 4 letters, print the result
+        print("The translation is \(word)")
+    }else if isAmerican == true { // If the word is an American word, translate it
+        //Reverse the word
+        let reversedWord = String(word.reversed())
+        for letter in reversedWord {
+            
+            //Add the letters into another string
+            translatedWord += String(letter)
+            //When the string has 1 letter, add u into the word.
+            if translatedWord.count == 1 {
+                translatedWord += "u"
+                
+                
+            }
+        }
+        // Reverse the word again and print the result.
+        let originalWord = String(translatedWord.reversed())
+        print("The translation is \(originalWord)")
+    }
 }
+
+
+
+
 
 // OUTPUT
 // Report results to the user here
